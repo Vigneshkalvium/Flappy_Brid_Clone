acc2a658142669f16126e80fa3fe90acc184ab3d
Vigneshkalvium <vipvignesh.dr.2244@gmail.com>
2025-03-27 14:17:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-amber-200">
+      <h1 className="text-3xl font-bold underline">Hello world!!</h1>
+    </div>
+  )
+}
+
+export default App
