9f40a7a6dc9d40afccf1dc9234ed9ba13489a4e6
Vigneshkalvium <vipvignesh.dr.2244@gmail.com>
2025-04-02 17:40:27 +0530

    working on gamePage

--- src/components/StatrtPage.jsx
+++ src/components/StatrtPage.jsx
@@ -3,30 +3,47 @@ import backGround from '../assets/images/background.jpg'
 import bird from '../assets/images/bird.lottie'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { motion } from "motion/react"
-import { div } from 'framer-motion/client';
-const StatrtPage = () => {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+const StartPage = () => {
+  const navigate = useNavigate();
+  const [fly, setFly] = useState(false);
+
+  const handleStart = () => {
+    setFly(true);
+    setTimeout(() => {
+      navigate('/game');
+    }, 900);
+  };
+
   return (
     <div className='relative overflow-hidden bg-cyan-200 h-screen w-screen flex justify-center items-center top-0'>
-      <img src={backGround} alt="" className='absolute inset-0 h-full w-full object-fit' />
+      <img src={backGround} alt="" className='absolute inset-0 h-full w-full object-cover' />
       <div className='absolute inset-0 flex flex-col justify-center items-center'>
         <motion.div
-          initial={{ x:"-100vw",y:"100vh",opacity: 0 }}
-          animate={{  x:"0",y:"0",opacity: 1 }}
-          transition = {{ duration: 2 }}  
+          initial={{ x: "-100vw", y: "100vh", opacity: 0 }}
+          animate={fly ? { x: "100vw", y: "-50vh", opacity: 1 } : { x: "0", y: "0", opacity: 1 }}
+          transition={fly?{ duration: 2 }:{ duration: 2 }}
         >
           <DotLottieReact
             className='w-66 h-56'
             src={bird}
             loop
-            autoplay/>
+            autoplay
+          />
         </motion.div>
-        <button className='w-46 h-14 bg-red-700 border-1 border-red-950 rounded-2xl text-4xl uppercase font-bold hover:bg-red-500 text-center hover:scale-110'>
+        <motion.button
+          initial={{ y: "100vh", opacity: 0 }}
+          animate={fly?{opacity: 0}:{ y: "0", opacity: 1 }}
+          transition={{ duration: 2 }}
+          className='w-46 h-14 bg-red-700 border-1 border-red-950 rounded-2xl text-4xl uppercase font-bold hover:bg-red-500 text-center hover:scale-110 mt-5'
+          onClick={handleStart}
+        >
           Start
-        </button>
+        </motion.button>
       </div>
-  </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default StatrtPage
+export default StartPage;
